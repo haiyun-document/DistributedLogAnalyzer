e78f2ce20c342d6248c8a59f417df1509b6764b6
Daniel Rashid <drashid@squarespace.com>
2012-03-04 14:14:09 -0500

    Creating new sub-modules, some renaming, moving basic parser to core module, etc.

---
+++ LogPipeline/src/main/java/com/github/drashid/RedisSpout.java
@@ -0,0 +1,28 @@
+package com.github.drashid;
+
+import java.util.Map;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.IRichSpout;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+
+
+public class RedisSpout extends BaseRichSpout {
+
+  public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void nextTuple() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
